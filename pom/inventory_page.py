7658481e7587e38b5392659c83426a0cd8ffccd7
Przemek <przemyslaw.makowka@o2.pl>
2026-03-30 22:29:03 +0200

    Negative loging cases

--- pom/inventory_page.py
+++ pom/inventory_page.py
@@ -1,11 +1,18 @@
-from playwright.sync_api import Page, expect
+import allure
+from playwright.sync_api import Page
+
 from pom.base_page import BasePage
 
+
 class InventoryPage(BasePage):
+    URL = "https://www.saucedemo.com/inventory.html"
+
     def __init__(self, page: Page):
         super().__init__(page)
         self._primary_header = page.get_by_test_id("primary-header")
         self._app_logo = self._primary_header.locator(".app_logo")
 
-    def assert_app_logo_should_have_text(self, expected_text: str):
-        self.assert_element_should_have_text(locator=self._app_logo, expected_text=expected_text)
+    def assert_user_logged_on_inventory_page(self, expected_title_page_text: str):
+        with allure.step(f"Assert that user logged into the shop"):
+            self.assert_element_should_have_text(locator=self._app_logo, expected_text=expected_title_page_text)
+            self.assert_page_has_url(expected_url=self.URL)
--- test_login.py
+++ test_login.py
@@ -1,37 +1,62 @@
 import allure
 import pytest
 
-from pom.login_page import LoginPage
 from pom.inventory_page import InventoryPage
+from pom.login_page import LoginPage
+
 
 @allure.parent_suite("Saucedemo tests")
 @allure.suite("Login tests")
-
 @pytest.mark.parametrize(
-    "user_key, is_success",
+    "user_key",
     [
-        pytest.param("standard_user", True, id="Standard user"),
-        pytest.param("locked_out_user", False, id="Locked out user"),
-        pytest.param("problem_user", True, id="Problem user"),
-        pytest.param("performance_glitch_user", True, id="Performance glitch_user"),
-        pytest.param("error_user", True, id="Error user"),
-        pytest.param("visual_user", True, id="Visual user"),
+        pytest.param("standard_user", id="Standard user"),
+        pytest.param("problem_user", id="Problem user"),
+        pytest.param("performance_glitch_user", id="Performance glitch_user"),
+        pytest.param("error_user", id="Error user"),
+        pytest.param("visual_user", id="Visual user"),
     ]
 )
-def test_login(
+def test_login_positive_cases(
         login_page: LoginPage,
         inventory_page: InventoryPage,
         credentials: dict[str, str],
         user_key: str,
-        is_success: bool
 ):
     username = credentials[user_key]
     password = credentials["password"]
-    login_page.navigate_to_login_page()
     login_page.login(username=username, password=password)
-    if is_success:
-        expected_text = "Swag Labs"
-        inventory_page.assert_app_logo_should_have_text(expected_text)
-    else:
-        expected_text = "Epic sadface: Sorry, this user has been locked out."
-        login_page.assert_message_container_should_have_text(expected_text)
+    expected_text = "Swag Labs"
+    inventory_page.assert_user_logged_on_inventory_page(expected_title_page_text=expected_text)
+
+
+@allure.parent_suite("Saucedemo tests")
+@allure.suite("Login tests")
+@allure.title("Locked out user")
+def test_login_locked_user(login_page: LoginPage, credentials: dict[str, str]):
+    username = credentials["locked_out_user"]
+    password = credentials["password"]
+    login_page.login(username=username, password=password)
+    expected_error_message = "Epic sadface: Sorry, this user has been locked out."
+    login_page.assert_error_message_container_should_have_text(expected_error_message)
+
+
+@allure.parent_suite("Saucedemo tests")
+@allure.suite("Login tests")
+@pytest.mark.parametrize(
+    "username, password, expected_error_message",
+    [
+        pytest.param("", "", "Epic sadface: Username is required", id="Empty username and password"),
+        pytest.param("wrong_user", "", "Epic sadface: Password is required", id="Username and empty password"),
+        pytest.param("", "wrong_password", "Epic sadface: Username is required", id="Performance glitch_user"),
+        pytest.param(
+            "wrong_user",
+            "wrong_password",
+            "Epic sadface: Username and password do not match any user in this service",
+            id="Error user"
+        ),
+    ]
+)
+def test_login_negative_cases(login_page: LoginPage, username: str, password: str, expected_error_message: str):
+    login_page.login(username=username, password=password)
+    login_page.assert_error_message_container_should_have_text(expected_error_message)
